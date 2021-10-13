5a91a4c07e2f5bf927ab764096bf843e93ce7ce6
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-10-13 08:22:55 -0700

    Basic website with parcel

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { FC } from 'react'
+import dot from './dot.png'
+
+const App: FC = () => {
+  return (
+    <>
+      Hello <br />
+      <img src={dot} alt='Dot' />
+    </>
+  )
+}
+
+export default App
---
+++ index.tsx
@@ -0,0 +1,5 @@
+import { render } from 'react-dom'
+import App from './App'
+
+const app = document.getElementById('app')
+render(<App />, app)
