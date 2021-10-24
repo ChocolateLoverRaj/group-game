861d9710789ef32969d76b2a92f6ba2934305874
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-10-24 08:32:47 -0700

    Add tip about switching to http

--- common/JoinWs.tsx
+++ common/JoinWs.tsx
@@ -1,10 +1,13 @@
 import { FC, useContext } from 'react'
 import { OnJoin, Validate } from './Api'
-import { Form, Input, Button } from 'antd'
+import { Form, Input, Button, Collapse, Typography } from 'antd'
 import defaultWsPort from '../electron/defaultWsPort'
 import WsClientService from './WsClientService'
 import NameTakenError from './NameTakenError'
 import Context from './Context'
+import { WarningOutlined } from '@ant-design/icons'
+
+const { Text } = Typography
 
 export interface JoinTcpProps {
   validate: Validate
@@ -44,9 +47,33 @@ const JoinWs: FC<JoinTcpProps> = ({ validate, onJoin }) => {
         >
           <Input />
         </Form.Item>
-        <Form.Item>
-          You may need to {getLink('https://experienceleague.adobe.com/docs/target/using/experiences/vec/troubleshoot-composer/mixed-content.html?lang=en', 'allow mixed content in your browser')}
-        </Form.Item>
+        {window.location.protocol === 'https:' && (
+          <Form.Item>
+            <Text type='danger'>
+              <WarningOutlined /> This page was loaded using the
+              <Text code type='danger'>HTTPS</Text> protocol.
+              Your browser may block requests that use the <Text code type='danger'>WS</Text>
+              protocol.
+            </Text>
+            <br />
+            <Collapse ghost>
+              <Collapse.Panel header='Possible Solutions' key='possibleSolutions'>
+                <ul>
+                  <li>
+                    Switch to the
+                    <a href={`http://${window.location.host}`}>
+                      <Text code>HTTP</Text> version of this site
+                    </a>
+                  </li>
+                  <li>
+                    {getLink(
+                      'https://experienceleague.adobe.com/docs/target/using/experiences/vec/troubleshoot-composer/mixed-content.html?lang=en',
+                      'Allow mixed content in your browser')}
+                  </li>
+                </ul>
+              </Collapse.Panel>
+            </Collapse>
+          </Form.Item>)}
         <Form.Item>
           <Button htmlType='submit'>Connect</Button>
         </Form.Item>
