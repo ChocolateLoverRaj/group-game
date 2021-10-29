d7b634a9a6bbd0e81cd3c911c427df023bd3cbef
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2021-10-29 14:29:11 -0700

    Show loading while joining

--- common/JoinSocket.tsx
+++ common/JoinSocket.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from 'react'
+import { FC, ReactNode, useState } from 'react'
 import { OnJoin, Validate } from './Api'
 import { Form, Input, Button } from 'antd'
 import NameTakenError from './NameTakenError'
@@ -21,40 +21,48 @@ const JoinSocket: FC<JoinSocketProps> = ({
   onJoin,
   additionalFormItems,
   defaultPort
-}) => (
-  <Form>
-    <Form.Item
-      name='addressPort'
-      label='Server Address (Including Port)'
-      required
-      rules={[{ required: true }, {
-        validateTrigger: 'finish',
-        validator: async (_rule, addressPort) => {
-          if (!await validate() || typeof addressPort !== 'string') return
-          const [address, portStr] = addressPort.split(':')
-          const port = portStr !== undefined ? parseInt(portStr) : defaultPort
-          if (!Number.isInteger(port)) {
-            throw new Error(`'${portStr}' is not a valid port`)
+}) => {
+  const [form] = Form.useForm()
+  const [joining, setJoining] = useState(false)
+
+  return (
+    <Form form={form}>
+      <Form.Item
+        name='addressPort'
+        label='Server Address (Including Port)'
+        required
+        rules={[{ required: true }, {
+          validateTrigger: 'finish',
+          validator: async (_rule, addressPort) => {
+            setJoining(true)
+            await (async () => {
+              if (!await validate() || typeof addressPort !== 'string') return
+              const [address, portStr] = addressPort.split(':')
+              const port = portStr !== undefined ? parseInt(portStr) : defaultPort
+              if (!Number.isInteger(port)) {
+                throw new Error(`'${portStr}' is not a valid port`)
+              }
+              const service: SocketClientService = new Service(address, port)
+              try {
+                await service.start()
+              } catch (e) {
+                if (e instanceof NameTakenError) throw e
+                console.error('Error connecting', e)
+                throw new Error('Error connecting')
+              }
+              onJoin(service)
+            })().finally(setJoining.bind(undefined, false))
           }
-          const service: SocketClientService = new Service(address, port)
-          try {
-            await service.start()
-          } catch (e) {
-            if (e instanceof NameTakenError) throw e
-            console.error('Error connecting', e)
-            throw new Error('Error connecting')
-          }
-          onJoin(service)
-        }
-      }]}
-    >
-      <Input />
-    </Form.Item>
-    {additionalFormItems}
-    <Form.Item>
-      <Button htmlType='submit'>Connect</Button>
-    </Form.Item>
-  </Form>
-)
+        }]}
+      >
+        <Input readOnly={joining} />
+      </Form.Item>
+      {additionalFormItems}
+      <Form.Item>
+        <Button htmlType='submit' loading={joining}>Connect</Button>
+      </Form.Item>
+    </Form>
+  )
+}
 
 export default JoinSocket
